30913028d43a14965a111236639f940151b6869b
ToHellForever <dim.anosoff2018@yandex.ru>
2025-11-22 12:10:23 +0300

    подчистил шаблон, перенёс скрипты в main.js

--- static/js/main.js
+++ static/js/main.js
@@ -4,13 +4,17 @@
 
     // Функция для открытия модального окна
     window.openOrderModal = function() {
-      modal.style.display = 'block';
+      if (modal) {
+        modal.style.display = 'block';
+      }
     };
 
     // Закрытие модального окна
     if (closeBtn) {
       closeBtn.onclick = function() {
-        modal.style.display = 'none';
+        if (modal) {
+          modal.style.display = 'none';
+        }
       };
     }
 
@@ -20,4 +24,36 @@
         modal.style.display = 'none';
       }
     };
+
+    // Функция для отображения блоков информации
+    window.showInfo = function(id) {
+      const selectedBlock = document.getElementById(id);
+      if (selectedBlock) {
+        if (selectedBlock.style.display === 'block') {
+          selectedBlock.style.display = 'none';
+        } else {
+          // Hide all info blocks
+          const infoBlocks = document.querySelectorAll('.info-block');
+          infoBlocks.forEach(block => {
+            block.style.display = 'none';
+          });
+
+          // Show the selected info block
+          selectedBlock.style.display = 'block';
+        }
+      }
+    };
+
+    // Обработчики для кнопок с классом .button
+    const buttons = document.querySelectorAll(".button");
+    if (buttons.length > 0) {
+      buttons.forEach(button => {
+        const img = button.querySelector(".arrow_accorderon");
+        if (img) {
+          button.addEventListener("click", () => {
+            img.classList.toggle("active");
+          });
+        }
+      });
+    }
   });
