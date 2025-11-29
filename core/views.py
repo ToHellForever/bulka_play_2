771bdacae944ea4ce2d3a66172babbe29e756224
ToHellForever <dim.anosoff2018@yandex.ru>
2025-11-29 15:52:15 +0300

    модальное окно с каруселью для новостей

--- core/views.py
+++ core/views.py
@@ -4,8 +4,18 @@ from django.http import JsonResponse
 from django.contrib import messages
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
-from .models import Product, Arenda, News, Order, PlayerRange, Size, PlayerCount, PlayerAge, GameType, AdditionalProducts
-
+from .models import (
+    Product,
+    Arenda,
+    News,
+    Order,
+    PlayerRange,
+    Size,
+    PlayerCount,
+    PlayerAge,
+    GameType,
+    AdditionalProducts,
+)
 
 
 class LandingView(TemplateView):
@@ -13,31 +23,46 @@ class LandingView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["products"] = Product.objects.filter(is_active=True).order_by("-created_at")
+        context["products"] = Product.objects.filter(is_active=True).order_by(
+            "-created_at"
+        )
         context["active_product_count"] = Product.objects.filter(is_active=True).count()
-        context["arenda"] = Arenda.objects.filter(is_active=True).order_by("-created_at")
-        context["additional_products"] = AdditionalProducts.objects.filter(is_active=True).order_by("-created_at")
+        context["arenda"] = Arenda.objects.filter(is_active=True).order_by(
+            "-created_at"
+        )
+        context["additional_products"] = AdditionalProducts.objects.filter(
+            is_active=True
+        ).order_by("-created_at")
         return context
+
 
 class AboutView(TemplateView):
     template_name = "about.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["products"] = Product.objects.filter(is_active=True).order_by("-created_at")
-        context["additional_products"] = AdditionalProducts.objects.filter(is_active=True).order_by("-created_at")
-        context["arenda"] = Arenda.objects.filter(is_active=True).order_by("-created_at")
+        context["products"] = Product.objects.filter(is_active=True).order_by(
+            "-created_at"
+        )
+        context["additional_products"] = AdditionalProducts.objects.filter(
+            is_active=True
+        ).order_by("-created_at")
+        context["arenda"] = Arenda.objects.filter(is_active=True).order_by(
+            "-created_at"
+        )
         context["news"] = News.objects.filter(is_active=True).order_by("-created_at")
         return context
-    
-    
+
+
 class GameCatalogView(TemplateView):
     template_name = "game_catalog.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         products = Product.objects.filter(is_active=True)
-        context["additional_products"] = AdditionalProducts.objects.filter(is_active=True).order_by("-created_at")
+        context["additional_products"] = AdditionalProducts.objects.filter(
+            is_active=True
+        ).order_by("-created_at")
 
         # Получаем все возможные значения для фильтров
         sizes = Size.objects.all()
@@ -46,35 +71,37 @@ class GameCatalogView(TemplateView):
         game_types = GameType.objects.all()
 
         # Обработка поискового запроса
-        search_query = self.request.GET.get('search', '')
+        search_query = self.request.GET.get("search", "")
         if search_query:
-            products = products.filter(name__iregex=r'{}'.format(search_query))
+            products = products.filter(name__iregex=r"{}".format(search_query))
 
         # Обработка фильтров
-        if 'size' in self.request.GET and self.request.GET['size']:
-            products = products.filter(sizes__id=self.request.GET['size'])
+        if "size" in self.request.GET and self.request.GET["size"]:
+            products = products.filter(sizes__id=self.request.GET["size"])
 
-        if 'player_count' in self.request.GET and self.request.GET['player_count']:
-            products = products.filter(player_counts__id=self.request.GET['player_count'])
+        if "player_count" in self.request.GET and self.request.GET["player_count"]:
+            products = products.filter(
+                player_counts__id=self.request.GET["player_count"]
+            )
 
-        if 'player_age' in self.request.GET and self.request.GET['player_age']:
-            products = products.filter(player_ages__id=self.request.GET['player_age'])
+        if "player_age" in self.request.GET and self.request.GET["player_age"]:
+            products = products.filter(player_ages__id=self.request.GET["player_age"])
 
-        if 'game_type' in self.request.GET and self.request.GET['game_type']:
-            products = products.filter(game_types__id=self.request.GET['game_type'])
+        if "game_type" in self.request.GET and self.request.GET["game_type"]:
+            products = products.filter(game_types__id=self.request.GET["game_type"])
 
         # Обработка сортировки
-        sort = self.request.GET.get('sort', '')
-        if sort == 'price_asc':
-            products = products.order_by('price')
-        elif sort == 'price_desc':
-            products = products.order_by('-price')
-        elif sort == 'name_asc':
-            products = products.order_by('name')
-        elif sort == 'name_desc':
-            products = products.order_by('-name')
+        sort = self.request.GET.get("sort", "")
+        if sort == "price_asc":
+            products = products.order_by("price")
+        elif sort == "price_desc":
+            products = products.order_by("-price")
+        elif sort == "name_asc":
+            products = products.order_by("name")
+        elif sort == "name_desc":
+            products = products.order_by("-name")
         else:
-            products = products.order_by('-created_at')
+            products = products.order_by("-created_at")
 
         context["products"] = products
 
@@ -85,8 +112,8 @@ class GameCatalogView(TemplateView):
         context["game_types"] = game_types
 
         return context
-    
-    
+
+
 class ProductDetailView(TemplateView):
     template_name = "product_detail.html"
 
@@ -94,30 +121,47 @@ class ProductDetailView(TemplateView):
         context = super().get_context_data(**kwargs)
         current_product = Product.objects.get(pk=self.kwargs.get("pk"))
         context["product"] = current_product
-        context["products"] = Product.objects.filter(is_active=True).order_by("-created_at")
+        context["products"] = Product.objects.filter(is_active=True).order_by(
+            "-created_at"
+        )
         context["additional_images"] = current_product.additional_images.all()
 
-        random_products = list(Product.objects.filter(is_active=True).exclude(pk=current_product.pk).order_by('?')[:4]) 
-        context["random_products"] = random_products 
+        random_products = list(
+            Product.objects.filter(is_active=True)
+            .exclude(pk=current_product.pk)
+            .order_by("?")[:4]
+        )
+        context["random_products"] = random_products
         # Добавьте передачу данных аренды
-        context["arenda"] = Arenda.objects.filter(is_active=True).order_by("-created_at")
-        additional_products = list(AdditionalProducts.objects.filter(is_active=True).order_by('-created_at'))
+        context["arenda"] = Arenda.objects.filter(is_active=True).order_by(
+            "-created_at"
+        )
+        additional_products = list(
+            AdditionalProducts.objects.filter(is_active=True).order_by("-created_at")
+        )
         context["additional_products"] = additional_products
         return context
-    
-    
+
+
 class RentalCatalogView(TemplateView):
     template_name = "rental_catalog.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["additional_products"] = AdditionalProducts.objects.filter(is_active=True).order_by("-created_at")
-        context["products"] = Product.objects.filter(is_active=True).order_by("-created_at")
-        context["arenda"] = Arenda.objects.filter(is_active=True).order_by("-created_at")
+        context["additional_products"] = AdditionalProducts.objects.filter(
+            is_active=True
+        ).order_by("-created_at")
+        context["products"] = Product.objects.filter(is_active=True).order_by(
+            "-created_at"
+        )
+        context["arenda"] = Arenda.objects.filter(is_active=True).order_by(
+            "-created_at"
+        )
         context["news"] = News.objects.filter(is_active=True).order_by("-created_at")
         return context
 
-@method_decorator(csrf_exempt, name='dispatch')
+
+@method_decorator(csrf_exempt, name="dispatch")
 class ProcessOrderView(View):
     def post(self, request, *args, **kwargs):
         try:
@@ -125,56 +169,56 @@ class ProcessOrderView(View):
 
             # Создание заказа
             order = Order.objects.create(
-                name=data.get('name'),
-                phone=data.get('phone'),
-                order_type=data.get('order_type'),
-                comment=data.get('comment', '')
+                name=data.get("name"),
+                phone=data.get("phone"),
+                order_type=data.get("order_type"),
+                comment=data.get("comment", ""),
             )
 
             # Обработка в зависимости от типа заказа
-            if data.get('order_type') == 'buy':
+            if data.get("order_type") == "buy":
                 # Сохранение выбранных игр для покупки
-                if 'buy_games' in data:
-                    games = data.getlist('buy_games')
+                if "buy_games" in data:
+                    games = data.getlist("buy_games")
                     order.products.set(games)
 
                 # Сохранение дополнительных товаров
-                if 'additional_goods' in data:
-                    additional_goods = data.getlist('additional_goods')
+                if "additional_goods" in data:
+                    additional_goods = data.getlist("additional_goods")
                     order.additional_products.set(additional_goods)
 
                 # Сохранение адреса доставки
-                order.delivery_address = data.get('delivery_address')
+                order.delivery_address = data.get("delivery_address")
 
                 # Сохранение информации о гравировке
-                order.engraving = data.get('engraving', 'no')
+                order.engraving = data.get("engraving", "no")
 
-            elif data.get('order_type') == 'rent':
+            elif data.get("order_type") == "rent":
                 # Сохранение выбранных игр для аренды
-                if 'rent_games' in data:
-                    games = data.getlist('rent_games')
+                if "rent_games" in data:
+                    games = data.getlist("rent_games")
                     order.games_for_rent.set(games)
 
                 # Сохранение типа аренды
-                if 'rent_type' in data:
-                    order.arenda.set([data.get('rent_type')])
+                if "rent_type" in data:
+                    order.arenda.set([data.get("rent_type")])
 
                 # Сохранение даты аренды
-                if 'rent_date' in data:
-                    order.date = data.get('rent_date')
+                if "rent_date" in data:
+                    order.date = data.get("rent_date")
 
                 # Сохранение адреса доставки
-                order.delivery_address = data.get('rent_address')
+                order.delivery_address = data.get("rent_address")
 
             order.save()
 
-            return JsonResponse({
-                'success': True,
-                'message': 'Заказ успешно оформлен!'
-            })
+            return JsonResponse({"success": True, "message": "Заказ успешно оформлен!"})
 
         except Exception as e:
-            return JsonResponse({
-                'success': False,
-                'message': f'Ошибка при оформлении заказа: {str(e)}'
-            }, status=400)
+            return JsonResponse(
+                {
+                    "success": False,
+                    "message": f"Ошибка при оформлении заказа: {str(e)}",
+                },
+                status=400,
+            )
