50ae2921aeb08a4f9f42f999e7c9d4db6a6abdff
ToHellForever <dim.anosoff2018@yandex.ru>
2025-11-30 12:13:36 +0300

    настроил калькулятор для rental_catalog

--- static/js/rental_calculator.js
+++ static/js/rental_calculator.js
@@ -5,33 +5,29 @@ document.addEventListener('DOMContentLoaded', function() {
     const guestMaxCount = document.getElementById("guestMaxCount");
 
     function updateRecommendedGames(guests) {
+        if (guests < 2) {
+            guestMinCount.textContent = '';
+            guestMaxCount.textContent = '';
+            document.getElementById("gamesLabel").textContent = '';
+            return;
+        }
         fetch('/calculate_games/?guests=' + guests)
             .then(response => response.json())
             .then(data => {
                 guestMinCount.textContent = `${data.min} `;
                 guestMaxCount.textContent = ` ${data.max}`;
+                document.getElementById("gamesLabel").textContent = 'игр';
             })
             .catch(error => console.error('Ошибка:', error));
     };
 
-    // Ограничиваем минимальное значение двумя гостями
-    guestInput.addEventListener("change", () => {
-        let newValue = Math.max(parseInt(guestInput.value), 2); // Минимум - 2 человека
-        guestInput.value = newValue;
-        guestSlider.value = newValue;
-        updateRecommendedGames(newValue); // Отправляем запрос и обновляем рекомендации
-    });
-
-    // То же самое делаем для слайдера
+    // Обработка изменения слайдера
     guestSlider.addEventListener("input", () => {
         let newValue = Math.max(parseInt(guestSlider.value), 2); // Минимум - 2 человека
-        guestInput.value = newValue;
-        guestSlider.value = newValue;
+        const guestValueDisplay = document.getElementById("guestValueDisplay");
+        guestValueDisplay.style.display = "block";
+        guestValueDisplay.innerHTML = newValue + ' <span id="guestUnit">чел</span>';
         updateRecommendedGames(newValue); // Отправляем запрос и обновляем рекомендации
-    });
-
-    // Добавьте этот код для обновления --value
-    guestSlider.addEventListener("input", () => {
         const value = (guestSlider.value - guestSlider.min) / (guestSlider.max - guestSlider.min) * 100;
         guestSlider.style.setProperty('--value', `${value}%`);
     });
