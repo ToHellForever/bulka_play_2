b89f2a3d0bde9e75ee07b76eaf79ee140843b9cc
ToHellForever <dim.anosoff2018@yandex.ru>
2026-01-06 16:30:29 +0300

    502 ошибка

--- bulka_play_2/urls.py
+++ bulka_play_2/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+import debug_toolbar
 from django.contrib.sitemaps.views import sitemap
 from core.sitemaps import ProductSitemap, ArendaSitemap, NewsSitemap, StaticViewSitemap
 
@@ -64,4 +65,6 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
