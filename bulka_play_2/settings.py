582580704199aebbdfc0c574edbd89842e5f00d1
ToHellForever <dim.anosoff2018@yandex.ru>
2026-01-10 19:06:29 +0300

    забыл переопределить

--- bulka_play_2/settings.py
+++ bulka_play_2/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 TELEGRAM_BOT_API_KEY = os.getenv("TELEGRAM_BOT_API_KEY")
 TELEGRAM_USER_ID = os.getenv("TELEGRAM_USER_ID")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "195.133.194.40", "bulka-play.ru"]
 
